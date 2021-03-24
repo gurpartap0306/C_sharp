1fa5aa8f4379aba3911e6b5173dbd2bb8b468f7e
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2021-03-24 22:25:54 +0530

    added classes file with project on bank transactions

---
+++ classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var invalidAcoount = new BankAccount("invalid", -55);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            var account = new BankAccount("gurpartap", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            try
+            {
+                account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine( "Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+                Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
